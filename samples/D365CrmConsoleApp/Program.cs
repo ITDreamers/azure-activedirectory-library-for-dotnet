d97228f7081251b5e1d5d102d566901bc7f182b2
Ivan <home.peva@gmail.com>
2017-10-15 17:18:03 +0300

    added references to project

--- samples/D365CrmConsoleApp/Program.cs
+++ samples/D365CrmConsoleApp/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System.Configuration;
+using System.ServiceModel;
 
 namespace D365CrmConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static CrmServiceClient _client;
+
+        public static void Main(string[] args)
         {
+            try
+            {
+                CrmServiceClient _client = new
+                    CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString);
+
+                //Do stuff
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                string message = ex.Message;
+                throw;
+            }
         }
     }
 }
